4142782c11ed1ee524cd261ba4e15f01ca897e73
felixerdy <felix.erdmann@uni-muenster.de>
2020-07-27 21:27:48 +0200

    delete test file

--- test/felixtest.js
+++
@@ -1,6 +0,0 @@
-const boxCayenneLpp = require('./data/ttnBox_cayennelpp.json');
-const payloadCayenneLpp = require('./data/TTNpayload_cayennelpp.json');
-
-const decoder = require('../lib/decoding');
-
-decoder.decodeRequest({body: payloadCayenneLpp, box: boxCayenneLpp}).then(data => console.log(data))
